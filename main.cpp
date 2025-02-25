907e8625d8575d0bea82dbe5c540585e6e9850b3
Juan <jcestrella@uce.edu.ec>
2025-02-24 21:39:43 -0500

    fin

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
-#include <chrono>
 #include <iostream>
 #include <mpi.h>
 #include <fstream>
 #include <map>
-#include <string>
 #include <vector>
+#include <string>
 
 std::vector<int> read_file(){
     std::fstream fs("C:\\Users\\Lap\\Downloads\\datos.txt", std::ios::in);
@@ -28,7 +27,7 @@ std::vector<int> aplanar_tabla_frecuencias(const std::map<int, int>& map){
 
 std::map<int, int> desaplanar_tabla_frecuencias(const std::vector<int>& vector){
     std::map<int, int> respuesta;
-    for (int i = 0; i < vector.size(); i += 2){
+    for (size_t i = 0; i < vector.size(); i += 2){
         respuesta[vector[i]] = vector[i + 1];
     }
     return respuesta;
@@ -40,71 +39,65 @@ int main(int argc, char** argv){
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-    std::vector<int> vector = read_file();
-    std::map<int, int> tabla_frecuencias;
+    std::vector<int> vector;
+    if (rank == 0) {
+        vector = read_file();
+    }
 
     int num_elementos, resto;
-    std::vector<int> vector_elementos;
-    std::vector<int> vector_desplazamientos;
-    std::vector<int> vector_elementos_respuesta;
-    std::vector<int> vector_desplazamientos_respuesta;
-    std::vector<int> vector_respuestas;
-    std::vector<int> vector_local;
-    std::map<int, int> tabla_frecuencias_local;
+    std::vector<int> vector_elementos(nprocs, 0);
+    std::vector<int> vector_desplazamientos(nprocs, 0);
 
     if (rank == 0){
-        /*for (int i = 0; i < 1000; i++){
-            printf("%d\n", i);
-        }*/
         num_elementos = vector.size() / nprocs;
         resto = vector.size() % nprocs;
 
         for (int i = 0; i < nprocs; i++){
-            vector_elementos[i] = (rank == nprocs - 1) ? num_elementos + resto : num_elementos;
+            vector_elementos[i] = (i == nprocs - 1) ? num_elementos + resto : num_elementos;
         }
-        vector_desplazamientos[0] = 0;
         for (int i = 1; i < nprocs; i++){
-            vector_desplazamientos[i] = i * num_elementos;
+            vector_desplazamientos[i] = vector_desplazamientos[i - 1] + vector_elementos[i - 1];
         }
     }
 
+    // Broadcast para que todos los procesos conozcan cuántos elementos recibirán
+    MPI_Bcast(vector_elementos.data(), nprocs, MPI_INT, 0, MPI_COMM_WORLD);
+
+    std::vector<int> vector_local(vector_elementos[rank]);
+
+    // Distribuir los datos a cada proceso
     MPI_Scatterv(vector.data(), vector_elementos.data(), vector_desplazamientos.data(), MPI_INT,
-                 vector_local.data(), vector_elementos[rank], MPI_INT,
-                 0, MPI_COMM_WORLD);
+                 vector_local.data(), vector_elementos[rank], MPI_INT, 0, MPI_COMM_WORLD);
 
-
+    std::map<int, int> tabla_frecuencias_local;
     for (int numero : vector_local){
         tabla_frecuencias_local[numero]++;
     }
-    printf("Rank %d\n", rank);
-    printf("Valor | Frecuencia\n");
-    for (auto par : tabla_frecuencias_local){
-        printf("| %3d | %6d|\n", par.first, par.second);
-    }
 
     std::vector<int> tabla_frec_local_aplanada = aplanar_tabla_frecuencias(tabla_frecuencias_local);
-    int tamanio_tabla_frec_local_aplanada = tabla_frec_local_aplanada.size();
+    int tamanio_local = tabla_frec_local_aplanada.size();
 
-    MPI_Send(&tamanio_tabla_frec_local_aplanada,1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    std::vector<int> vector_elementos_respuesta(nprocs, 0);
+    std::vector<int> vector_desplazamientos_respuesta(nprocs, 0);
+    std::vector<int> vector_respuestas;
+
+    MPI_Gather(&tamanio_local, 1, MPI_INT, vector_elementos_respuesta.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     if (rank == 0){
-        for (int i = 0; i < nprocs; i++){
-            int valor;
-            MPI_Recv(&valor, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            vector_elementos_respuesta[i] = valor;
-        }
         vector_desplazamientos_respuesta[0] = 0;
         for (int i = 1; i < nprocs; i++){
             vector_desplazamientos_respuesta[i] = vector_desplazamientos_respuesta[i - 1] + vector_elementos_respuesta[i - 1];
         }
+        int total_size = vector_desplazamientos_respuesta[nprocs - 1] + vector_elementos_respuesta[nprocs - 1];
+        vector_respuestas.resize(total_size);
     }
 
-    MPI_Gatherv(tabla_frec_local_aplanada.data(), tamanio_tabla_frec_local_aplanada, MPI_INT,
-                vector_respuestas.data(), vector_elementos_respuesta.data(), vector_desplazamientos_respuesta.data(), MPI_INT,
-                0, MPI_COMM_WORLD);
+    MPI_Gatherv(tabla_frec_local_aplanada.data(), tamanio_local, MPI_INT,
+                vector_respuestas.data(), vector_elementos_respuesta.data(),
+                vector_desplazamientos_respuesta.data(), MPI_INT, 0, MPI_COMM_WORLD);
 
     if (rank == 0){
-        tabla_frecuencias = desaplanar_tabla_frecuencias(vector_respuestas);
+        std::map<int, int> tabla_frecuencias = desaplanar_tabla_frecuencias(vector_respuestas);
         printf("Valor | Frecuencia\n");
         for (auto par : tabla_frecuencias){
             printf("| %3d | %6d|\n", par.first, par.second);
